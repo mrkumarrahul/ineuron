9100055b64890b36c44730f85d5678cae9ae797d
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-09-29 09:52:17 +0530

    new version of virtual class

--- PurevirtualFunction.cpp
+++ PurevirtualFunction.cpp
@@ -26,27 +26,40 @@ class Triangle:public Shape
               return 0.5*x*y;
         }
     };
+    class square:public Shape
+    {
+      public:
+      float area(float x,float y)
+      {
+        return x*y;
+      }
+    };
     Shape * choice()
     {
         int c;
         cout<<"Enter your choice"<<endl;
         cout<<"Enter 1:Rectangle"<<endl;
         cout<<"Enter 2:Triangle"<<endl;
+        cout<<"Enter 3:square"<<endl;
         cin>>c;
         if(c==1)
         {
             return new Rectangle;
         }
-        else
+        else if(c==2)
         {
           return new Triangle;
-        }    
+        }  
+        else
+        {
+            return new square;
+        }
         
     }    
 int main()
 {
     Shape *p;
     p=choice();
-    cout<<"Area = "<<p->area(2,3)<<endl;
+    cout<<"Area = "<<p->area(3,3)<<endl;
     return 0;
 }
